2b970a81ed4f9a6eedb02dfc1621137779e2e664
andreas mewald <andreas.mewald@pdts.at>
2018-09-04 08:30:19 +0200

    Added DeviceGroupQuery actor

---
+++ AkkaNetIotDemo/Actors/DeviceGroupQuery.cs
@@ -0,0 +1,122 @@
+﻿using Akka.Actor;
+using Akka.Event;
+using AkkaNetIotDemo.Protocol;
+using System;
+using System.Collections.Generic;
+
+namespace AkkaNetIotDemo.Actors
+{
+    public class DeviceGroupQuery : UntypedActor
+    {
+        private ICancelable queryTimeoutTimer;
+
+        public DeviceGroupQuery(
+            Dictionary<IActorRef, string> actorToDeviceId,
+            long requestId,
+            IActorRef requester,
+            TimeSpan timeout)
+        {
+            ActorToDeviceId = actorToDeviceId ?? throw new ArgumentNullException(nameof(actorToDeviceId));
+            RequestId = requestId;
+            Requester = requester ?? throw new ArgumentNullException(nameof(requester));
+            Timeout = timeout;
+
+            queryTimeoutTimer = Context.System.Scheduler.ScheduleTellOnceCancelable(timeout, Self, CollectionTimeout.Instance, Self);
+
+            Become(WaitingForReplies(new Dictionary<string, ITemperatureReading>(), new HashSet<IActorRef>(actorToDeviceId.Keys)));
+        }
+
+        public Dictionary<IActorRef, string> ActorToDeviceId { get; }
+        public IActorRef Requester { get; }
+        public long RequestId { get; }
+        public TimeSpan Timeout { get; }
+
+        protected ILoggingAdapter Log { get; } = Context.GetLogger();
+
+        public static Props Props(
+            Dictionary<IActorRef, string> actorToDevice,
+            long requestId,
+            IActorRef requester,
+            TimeSpan timeout)
+            => Akka.Actor.Props.Create(() => new DeviceGroupQuery(actorToDevice, requestId, requester, timeout));
+
+        public void ReceivedResponse(
+            IActorRef deviceActor,
+            ITemperatureReading reading,
+            HashSet<IActorRef> stillWaiting,
+            Dictionary<string, ITemperatureReading> repliesSoFar)
+        {
+            Context.Unwatch(deviceActor);
+            var deviceId = ActorToDeviceId[deviceActor];
+            stillWaiting.Remove(deviceActor);
+
+            repliesSoFar.Add(deviceId, reading);
+            if (stillWaiting.Count == 0)
+            {
+                Requester.Tell(new RespondAllTemperatures(RequestId, repliesSoFar));
+                Context.Stop(Self);
+            }
+            else
+            {
+                Context.Become(WaitingForReplies(repliesSoFar, stillWaiting));
+            }
+        }
+
+        public UntypedReceive WaitingForReplies(
+            Dictionary<string, ITemperatureReading> repliesSoFar, HashSet<IActorRef> stillWaiting)
+        {
+            return message =>
+            {
+               switch (message)
+               {
+                   case RespondTemperature response when response.RequestId == 0:
+                       var deviceActor = Sender;
+                       ITemperatureReading temperatureReading = null;
+                       if (response.Value.HasValue)
+                       {
+                           temperatureReading = new Temperature(response.Value.Value);
+                       }
+                       else
+                       {
+                           temperatureReading = TemperatureNotAvailable.Instance;
+                       }
+
+                       ReceivedResponse(deviceActor, temperatureReading, stillWaiting, repliesSoFar);
+                       break;
+
+                   case Terminated terminated:
+                       ReceivedResponse(terminated.ActorRef, DeviceNotAvailable.Instance, stillWaiting, repliesSoFar);
+                       break;
+
+                   case CollectionTimeout collectionTimeout:
+                       var replies = new Dictionary<string, ITemperatureReading>(repliesSoFar);
+                       foreach (var actor in stillWaiting)
+                       {
+                           var deviceId = ActorToDeviceId[actor];
+                           replies.Add(deviceId, DeviceTimeOut.Instance);
+                       }
+                       Requester.Tell(new RespondAllTemperatures(RequestId, replies));
+                       Context.Stop(Self);
+
+
+                       break;
+               }
+           };
+        }
+
+        protected override void OnReceive(object message)
+        {
+        }
+
+        protected override void PostStop() => queryTimeoutTimer.Cancel();
+
+        protected override void PreStart()
+        {
+            foreach (var deviceActor in ActorToDeviceId.Keys)
+            {
+                Context.Watch(deviceActor);
+                deviceActor.Tell(new ReadTemperature(0, null));
+            }
+        }
+    }
+}
---
+++ AkkaNetIotDemo/Protocol/DeviceGroup.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+
+namespace AkkaNetIotDemo.Protocol
+{
+    public sealed class RequestAllTemperatures
+    {
+        public RequestAllTemperatures(long requestId)
+        {
+            RequestId = requestId;
+        }
+
+        public long RequestId { get; }
+    }
+
+    public sealed class RespondAllTemperatures
+    {
+        public RespondAllTemperatures(long requestId, Dictionary<string, ITemperatureReading> temperatures)
+        {
+            RequestId = requestId;
+            Temperatures = temperatures;
+        }
+
+        public long RequestId { get; }
+        public Dictionary<string, ITemperatureReading> Temperatures { get; }
+    }
+
+    public sealed class CollectionTimeout
+    {
+        public static CollectionTimeout Instance { get; } = new CollectionTimeout();
+
+        private CollectionTimeout()
+        {
+        }
+    }
+
+    public interface ITemperatureReading
+    {
+    }
+
+    public sealed class Temperature : ITemperatureReading
+    {
+        public Temperature(double value)
+        {
+            Value = value;
+        }
+
+        public double Value { get; }
+    }
+
+    public sealed class TemperatureNotAvailable : ITemperatureReading
+    {
+        public static TemperatureNotAvailable Instance { get; } = new TemperatureNotAvailable();
+
+        private TemperatureNotAvailable()
+        {
+        }
+    }
+
+    public sealed class DeviceNotAvailable : ITemperatureReading
+    {
+        public static DeviceNotAvailable Instance { get; } = new DeviceNotAvailable();
+
+        private DeviceNotAvailable()
+        {
+        }
+    }
+
+    public sealed class DeviceTimeOut : ITemperatureReading
+    {
+        public static DeviceTimeOut Instance { get; } = new DeviceTimeOut();
+
+        private DeviceTimeOut()
+        {
+        }
+    }
+}
